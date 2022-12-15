89d6e872c21259bf309e3bd7bb83bf68276def45
manzanofp <felipemanzanofp@gmail.com>
2022-12-15 17:40:10 -0300

    add JPA annotations for Doctor class

--- src/main/java/com/manzanofp/voli/med/api/controller/DoctorController.java
+++ src/main/java/com/manzanofp/voli/med/api/controller/DoctorController.java
@@ -1,6 +1,6 @@
 package com.manzanofp.voli.med.api.controller;
 
-import com.manzanofp.voli.med.api.doctor.dataRegisterDoctor;
+import com.manzanofp.voli.med.api.doctor.DataRegisterDoctor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DoctorController {
 
     @PostMapping
-    public void register(@RequestBody dataRegisterDoctor data){
+    public void register(@RequestBody DataRegisterDoctor data){
         System.out.println(data);
     }
 
--- src/main/java/com/manzanofp/voli/med/api/doctor/DataRegisterDoctor.java
+++ src/main/java/com/manzanofp/voli/med/api/doctor/DataRegisterDoctor.java
@@ -2,7 +2,7 @@ package com.manzanofp.voli.med.api.doctor;
 
 import com.manzanofp.voli.med.api.adress.DataAddress;
 
-public record dataRegisterDoctor(String name,
+public record DataRegisterDoctor(String name,
                                  String email,
                                  String crm,
                                  Specialty specialty,
